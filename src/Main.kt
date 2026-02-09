1b70e123539fd859e8e83c3179920e8372087860
beni <bendzy.thx@gmail.com>
2026-02-09 09:39:47 +0100

    Added classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+}
+
+
+
+
+data class Passenger(val name:String, val age:Int, val hasPriorityBoarding:Boolean, val luggageWeight:Double) {
+
+}
+
+abstract class Flight(
+    val flightNumber:String,
+    val destination:String) {
+
+    abstract fun calculateTicketPrice(p: Passenger):Double
+}
+
+class EconomyFlight(
+    flightNumber: String,
+    destination: String,
+    val ticket: Double = 100.0
+) : Flight(flightNumber, destination) {
+
+    override fun calculateTicketPrice(p: Passenger): Double {
+        return ticket
     }
 }
+
+class BussinessFlight (
+    flightNumber: String,
+    destination: String,
+    val ticket: Double = 100.0
+) : Flight(flightNumber, destination) {
+
+    override fun calculateTicketPrice(p: Passenger): Double {
+        return ticket + (p.luggageWeight * 20)
+    }
+}
+
+
+
